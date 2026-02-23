5b00672839afb00cf5d683e0d07fc5ec054092e4
llf-cn <61875916+llf-cn@users.noreply.github.com>
2026-02-23 18:09:01 +0800

    Change default IP and update OpenClash setup
    Updated default backend IP and modified OpenClash source handling.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,42 +1,17 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 修改默认后台 IP 为 192.168.0.1
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-#!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# 1. 添加 OpenClash 源码
+# 2. 拉取最新的 OpenClash 源码
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
-# 2. 修正核心依赖（确保可以使用最新的 OpenClash 和 IPv6 功能）
+# 3. 强制使用 dnsmasq-full 满足 OpenClash 需求
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 
-# 3. 设置默认 IP（可选，如果想改默认后台地址，把下面 192.168.1.1 改掉）
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
-
-# 在 diy-part2.sh 末尾添加
+# 4. 强制在编译前从配置中剔除所有无线包定义（双重保险，防止 hostapd 报错）
 sed -i '/wpad/d' .config
 sed -i '/hostapd/d' .config
 sed -i '/kmod-ath/d' .config
