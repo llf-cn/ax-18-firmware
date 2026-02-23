160208b14f0b7d0b1a692bbb3647518a2823ebae
llf-cn <61875916+llf-cn@users.noreply.github.com>
2026-02-23 16:10:54 +0800

    Update diy-part2.sh for OpenClash integration
    Add OpenClash source code and adjust dependencies

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,20 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# 1. 添加 OpenClash 源码
+rm -rf feeds/luci/applications/luci-app-openclash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
+# 2. 修正核心依赖（确保可以使用最新的 OpenClash 和 IPv6 功能）
+sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
+
+# 3. 设置默认 IP（可选，如果想改默认后台地址，把下面 192.168.1.1 改掉）
+# sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
