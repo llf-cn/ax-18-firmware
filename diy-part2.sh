29ed29c823ed7a8d28246249b908fd68a3487127
llf-cn <61875916+llf-cn@users.noreply.github.com>
2026-02-27 18:35:29 +0800

    Refactor diy-part2.sh for clarity and updates

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-# File name: diy-part2.sh
 
-# 1. 修改默认后台 IP 为 192.168.0.1
+# 1. 修改默认后台 IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # 2. 拉取最新的 OpenClash 源码
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
-# 3. 强制使用 dnsmasq-full 满足 OpenClash 需求
+# 3. 强制使用 dnsmasq-full
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
 
-# 4. 强制在编译前从配置中剔除所有无线包定义（双重保险，防止 hostapd 报错）
-sed -i '/wpad/d' .config
-sed -i '/hostapd/d' .config
-sed -i '/kmod-ath/d' .config
+# 4. 修改默认时区为上海
+sed -i "s/'UTC'/'CST-8'\n\t\tset system.@system[-1].timezone='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
-# 5. 时区配置（添加到 diy-part2.sh）
-sed -i 's/CONFIG_TIME_ZONE="UTC"/CONFIG_TIME_ZONE="Asia\/Shanghai"/g' .config
+# 5. 从硬件定义层面删除无线依赖 (解决无线菜单残留的关键)
+sed -i '/wpad/d' target/linux/qualcommax/image/ipq60xx.mk
+sed -i '/hostapd/d' target/linux/qualcommax/image/ipq60xx.mk
+sed -i '/ath11k/d' target/linux/qualcommax/image/ipq60xx.mk
